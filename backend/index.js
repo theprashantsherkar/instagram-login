453d71e73e893f7788eb19042bef4c06571673b3
Prashant <prashantsherkar7515400@gmail.com>
2025-04-25 11:54:16 +0530

    setting up changes

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,21 @@ import { config } from 'dotenv';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import colors from 'colors';
-import AuthRoutes from './routes/auth.js'
-import CommentRoutes from './routes/comment.js'
 import cors from 'cors';
+import axios from 'axios';
+
+const INSTAGRAM_CLIENT_ID = 1659563081412659
+const INSTAGRAM_CLIENT_SECRET = "63649d86d9738a407e3651a867b47640"
+const REDIRECT_URI = "https://instagram-login-0ge0.onrender.com/instagram/callback"
+
+
+let userAccessToken = "";
 
 config({
     path:"./database/config.env"
 });
 
-export const app = express();
+const app = express();
 
 app.use(cors({
     origin:"http://localhost:5173",
@@ -24,8 +30,59 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 
-app.use('/api/v1/auth', AuthRoutes);
-app.use('/api/v1/comment', CommentRoutes);
+app.get('/', async (req, res) => {
+
+    const redirectUrl = `https://api.instagram.com/oauth/authorize?client_id=${INSTAGRAM_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=user_profile,user_media&response_type=code`;
+    console.log(`redirecting to: ${redirectUrl}`)
+    res.redirect(redirectUrl);
+  });
+
+
+app.post('/instagram/callback', async (req, res) => {
+    const code = req.query.code;
+    console.log(code);
+    console.log({
+      INSTAGRAM_CLIENT_ID,
+      INSTAGRAM_CLIENT_SECRET
+    })
+  
+    try {
+      const response = await axios.post(`https://api.instagram.com/oauth/access_token`, null, {
+        params: {
+          client_id: INSTAGRAM_CLIENT_ID,
+          client_secret: INSTAGRAM_CLIENT_SECRET,
+          grant_type: 'authorization_code',
+          redirect_uri: REDIRECT_URI,
+          code
+        }
+      });
+      console.log('second api passed')
+      userAccessToken = response.data.access_token;
+  
+      res.redirect(`http://localhost:5173?token=${userAccessToken}`);
+    } catch (err) {
+      res.status(500).json({ error: 'Token exchange failed', details: err.response?.data || err.message });
+    }
+  }
+    );
+
+
+app.get('/getPosts', async (req, res) => {
+    const token = req.query.token;
+    try {
+      const mediaRes = await axios.get(`https://graph.instagram.com/me/media`, {
+        params: {
+          fields: 'id,caption,media_url,permalink',
+          access_token: token
+        }
+      });
+      res.json(mediaRes.data);
+    } catch (err) {
+      res.status(500).json({ error: 'Fetching media failed', details: err.response?.data || err.message });
+    }
+  })
 
 
 
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => console.log(`server is running at port: ${port}`.bgMagenta.black.bold));
--- backend/server.js
+++
@@ -1,8 +0,0 @@
-import { app } from "./index.js";
-import { connectDB } from "./database/db.js";
-
-// connectDB();
-
-app.listen(process.env.PORT, () => {
-    console.log(`server is running on port: ${process.env.PORT}`.bgMagenta.black.bold)
-})          
