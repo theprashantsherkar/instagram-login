a4eb54b3b7c2b8148fa6661b1f8cf884b76dc09a
Prashant <prashantsherkar7515400@gmail.com>
2025-04-25 20:15:57 +0530

    imports changed

---
+++ backend/server.js
@@ -0,0 +1,4 @@
+const INSTAGRAM_CLIENT_ID = "1659563081412659"
+const INSTAGRAM_CLIENT_SECRET = "63649d86d9738a407e3651a867b47640"
+const SESSION_SECRET = "tbjanerfmksjnarokfrmkfvrnr"
+const REDIRECT_URI = "https://instagram-login-0ge0.onrender.com/instagram/callback"
